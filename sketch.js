7598d139a04c736420acf5084c556f796b43a919
myraabhatia <62111137+myraabhatia@users.noreply.github.com>
2020-05-24 10:14:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var Form;
+
+function setup(){
+var canvas = createCanvas(1200,1200);
+var database = firebase.database();
+
+}
+
+function draw(){
+background("pink");
+ 
+
+Form = new form();
+Form.display();
+
+}
